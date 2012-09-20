436b3fe641dac0aa27e0acf223e1a1153f87f670
ana <ana.devel@gmail.com>
2012-09-20 13:10:47 -0400

    add bulk arrange of leds

--- p3/tcl_helper.js
+++ p3/tcl_helper.js
@@ -26,241 +26,241 @@
     </div>
 */
 
-function led(diagram, id, top, left)
-{
-  this.diagram = diagram;
-  this.dom_id = diagram.dom_id + "_led" + id;
-
-  var dpos = diagram.obj.offset();
-  top += dpos.top;
-  left += dpos.left;
-
-  this.obj = $('<div class="led"/>').attr({
-      id: this.dom_id,
-      style: "position: absolute;" +
-              "top: "+ top +"px;" +
-              "left: " + left +"px;",
-      led_id: id
-  }).appendTo("#" + diagram.dom_id);
-  //alert(this.obj[0].id);
-
-  var options ={ 
-    containment: "parent", 
-    drag: function() {
-      diagram.draw();
-    }
-  };
-  this.obj.draggable(options);
-
-  this.canvas = $('<canvas />').attr({
-    height: "50px",
-    width: "50px"
-  }).appendTo(this.obj)[0];
-
-  // draw the circle
-  var context = this.canvas.getContext("2d");
-  context.fillStyle = "red";
-  context.strokeStyle = "#555555";
-  context.beginPath();
-  //draw arc: arc(x, y, radius, startAngle, endAngle, anticlockwise)
-  context.arc(25, 25, 10, Math.PI*2, 0, true);
-  context.closePath();
-  context.fill();
-  context.stroke();
-
-  this.change_color =
-  function(color)
+  function led(diagram, id, top, left)
   {
-    var context = this.canvas.getContext("2d");
-    context.fillStyle = color;
-    context.fill();
-  };
+    this.diagram = diagram;
+    this.dom_id = diagram.dom_id + "_led" + id;
 
-}
+    var dpos = diagram.obj.offset();
+    top += dpos.top;
+    left += dpos.left;
 
+    this.obj = $('<div class="led"/>').attr({
+        id: this.dom_id,
+        style: "position: absolute;" +
+                "top: "+ top +"px;" +
+                "left: " + left +"px;",
+        led_id: id
+    }).appendTo("#" + diagram.dom_id);
+    //alert(this.obj[0].id);
 
-//------------------------------
-// Diagram
-// has_many leds
-
-function diagram(dom_id, frameset_dom_id)
-{
-  this.dom_id = dom_id;
-  this.obj = $("#"+dom_id);
-  this.leds = new Array();
-  this.frames = new Array();
-  this.frames_target = $("#"+frameset_dom_id);
-
-  this.canvas = this.obj.find("canvas")[0]; 
-
-  this.clear = 
-  function clear()
-  {
-    var context = this.canvas.getContext("2d");
-    context.save();
-    context.setTransform(1, 0, 0, 1, 0, 0);
-    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    context.restore();
-  };
-
-  this.draw =
-  function draw() 
-  {
-    this.clear();
-    // redraw LED wiring
-    var context = this.canvas.getContext("2d");
-    var canvas_pos = this.obj.find("canvas").offset();
-    for(i = 1; i < this.leds.length; i++) {
-
-      var pos = this.leds[i-1].obj.offset();
-      var x1 = pos.left + this.leds[i-1].obj.width()/2 - canvas_pos.left;
-      var y1 = pos.top + this.leds[i-1].obj.height()/2 - canvas_pos.top;
-
-      var pos = this.leds[i].obj.offset();
-      var x2 = pos.left + this.leds[i].obj.width()/2 - canvas_pos.left;
-      var y2 = pos.top + this.leds[i].obj.height()/2 - canvas_pos.top;
-
-      context.beginPath();
-      context.moveTo(x1,y1);
-      context.lineTo(x2,y2);
-      context.stroke();
-
-    }
-  };
-
-  this.clear_frames =
-  function ()
-  {
-    this.frames_target.find("li.frame").remove();
-    this.frames = new Array();
-    this.append_frame();
-  };
-
-  this.append_frame =
-  function(append_after = null)
-  {
-    this.frames.push(new frame(this, this.frames_target, this.frames.length+1, append_after));
-    this.current_frame = this.frames.length - 1;
-    this.frames[this.current_frame].display();
-    this.update_selected(); 
-  };
-
-  this.duplicate_frame =
-  function()
-  {
-    var colors = this.frame().colors.slice(0);
-    this.append_frame(this.frame().obj);
-    this.frame().colors = colors;
-    this.frame().display();
-  };
-
-  this.initialize = 
-  function(n_leds)
-  {
-    var top = 0, left = 0;
-    var width = this.obj.width();
-
-    // remove existing leds
-    this.obj.find(".led").remove()
-    this.leds = new Array();
-    
-    // add leds
-    for(i = 0; i < n_leds; i++) {
-      this.leds.push(new led(this, i, top, left));
-      if(left+55 >= width) {
-        left = 0;
-        top += 55;
-      } else {
-        left += 55;
+    var options ={ 
+      containment: "parent", 
+      drag: function() {
+        diagram.draw();
       }
-    }
+    };
+    this.obj.draggable(options);
 
-    // make the leds droppable
-    $("div.led").droppable ({ 
-        drop: function(event, ui) {
-          //change_color($(this), ui.draggable[0].style.backgroundColor);
-          //change_color($(this), ui.draggable.css("background-color"));
-          $diagram.frame().change_color($(this).attr("led_id"), ui.draggable.css("background-color"));
-        },
-        accept: ".pot"
+    this.canvas = $('<canvas />').attr({
+      height: "50px",
+      width: "50px"
+    }).appendTo(this.obj)[0];
+
+    // draw the circle
+    var context = this.canvas.getContext("2d");
+    context.fillStyle = "red";
+    context.strokeStyle = "#555555";
+    context.beginPath();
+    //draw arc: arc(x, y, radius, startAngle, endAngle, anticlockwise)
+    context.arc(25, 25, 10, Math.PI*2, 0, true);
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+    this.change_color =
+    function(color)
+    {
+      var context = this.canvas.getContext("2d");
+      context.fillStyle = color;
+      context.fill();
+    };
+
+  }
+
+
+  //------------------------------
+  // Diagram
+  // has_many leds
+
+  function diagram(dom_id, frameset_dom_id)
+  {
+    this.dom_id = dom_id;
+    this.obj = $("#"+dom_id);
+    this.leds = new Array();
+    this.frames = new Array();
+    this.frames_target = $("#"+frameset_dom_id);
+
+    this.canvas = this.obj.find("canvas")[0]; 
+
+    this.clear = 
+    function clear()
+    {
+      var context = this.canvas.getContext("2d");
+      context.save();
+      context.setTransform(1, 0, 0, 1, 0, 0);
+      context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      context.restore();
+    };
+
+    this.draw =
+    function draw() 
+    {
+      this.clear();
+      // redraw LED wiring
+      var context = this.canvas.getContext("2d");
+      var canvas_pos = this.obj.find("canvas").offset();
+      for(i = 1; i < this.leds.length; i++) {
+
+        var pos = this.leds[i-1].obj.offset();
+        var x1 = pos.left + this.leds[i-1].obj.width()/2 - canvas_pos.left;
+        var y1 = pos.top + this.leds[i-1].obj.height()/2 - canvas_pos.top;
+
+        var pos = this.leds[i].obj.offset();
+        var x2 = pos.left + this.leds[i].obj.width()/2 - canvas_pos.left;
+        var y2 = pos.top + this.leds[i].obj.height()/2 - canvas_pos.top;
+
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+
+      }
+    };
+
+    this.clear_frames =
+    function ()
+    {
+      this.frames_target.find("li.frame").remove();
+      this.frames = new Array();
+      this.append_frame();
+    };
+
+    this.append_frame =
+    function(append_after = null)
+    {
+      this.frames.push(new frame(this, this.frames_target, this.frames.length+1, append_after));
+      this.current_frame = this.frames.length - 1;
+      this.frames[this.current_frame].display();
+      this.update_selected(); 
+    };
+
+    this.duplicate_frame =
+    function()
+    {
+      var colors = this.frame().colors.slice(0);
+      this.append_frame(this.frame().obj);
+      this.frame().colors = colors;
+      this.frame().display();
+    };
+
+    this.initialize = 
+    function(n_leds)
+    {
+      var top = 0, left = 0;
+      var width = this.obj.width();
+
+      // remove existing leds
+      this.obj.find(".led").remove()
+      this.leds = new Array();
+      
+      // add leds
+      for(i = 0; i < n_leds; i++) {
+        this.leds.push(new led(this, i, top, left));
+        if(left+55 >= width) {
+          left = 0;
+          top += 55;
+        } else {
+          left += 55;
+        }
+      }
+
+      // make the leds droppable
+      $("div.led").droppable ({ 
+          drop: function(event, ui) {
+            //change_color($(this), ui.draggable[0].style.backgroundColor);
+            //change_color($(this), ui.draggable.css("background-color"));
+            $diagram.frame().change_color($(this).attr("led_id"), ui.draggable.css("background-color"));
+          },
+          accept: ".pot"
+        });
+
+      // click on led to change color if global brush_color is set
+      $("div.led").click( function() {
+        if($brush_color) {
+          $diagram.frame().change_color($(this).attr("led_id"), $brush_color);
+        }
       });
 
-    // click on led to change color if global brush_color is set
-    $("div.led").click( function() {
-      if($brush_color) {
-        $diagram.frame().change_color($(this).attr("led_id"), $brush_color);
-      }
-    });
-
-    this.clear_frames();
-    this.draw();
-  }
-
-  this.frame =
-  function()
-  {
-    return this.frames[this.current_frame]; 
-  }
-
-  this.move_to_frame =
-  function(id)
-  {
-    for(i = 0; i< this.frames.length; i++) {
-      if(this.frames[i].id == id) {
-        break;
-      }
+      this.clear_frames();
+      this.draw();
     }
-    this.current_frame = i;
-    this.update_selected();
-  };
 
-  this.update_selected =
-  function()
-  {
-    $("#" + this.frames_target.attr("id") + " li.frame div").removeClass("selected");
-    this.frames[this.current_frame].obj.find("div").addClass("selected");
-    this.frames[this.current_frame].display();
-  }
+    this.frame =
+    function()
+    {
+      return this.frames[this.current_frame]; 
+    }
 
-  // return frames in user order
-  this.ordered_frames =
-  function ()
-  {
-    return this.frames;
-    //$("div.frame").map(function() { return $(this).attr("frame_id"); } );
-  }
+    this.move_to_frame =
+    function(id)
+    {
+      for(i = 0; i< this.frames.length; i++) {
+        if(this.frames[i].id == id) {
+          break;
+        }
+      }
+      this.current_frame = i;
+      this.update_selected();
+    };
 
-  this.export =
-  function()
-  {
-    var formatted_frames = new Array();
-    var formatted_colors = new Array();
+    this.update_selected =
+    function()
+    {
+      $("#" + this.frames_target.attr("id") + " li.frame div").removeClass("selected");
+      this.frames[this.current_frame].obj.find("div").addClass("selected");
+      this.frames[this.current_frame].display();
+    }
 
-    var frames = this.ordered_frames();
-    for(i = 0; i < frames.length; i ++) {
-      var colors = frames[i].colors;
-      for(j = 0; j < colors.length; j ++) {
-        formatted_colors[j] = format_color(colors[j]);
+    // return frames in user order
+    this.ordered_frames =
+    function ()
+    {
+      return this.frames;
+      //$("div.frame").map(function() { return $(this).attr("frame_id"); } );
+    }
+
+    this.export =
+    function()
+    {
+      var formatted_frames = new Array();
+      var formatted_colors = new Array();
+
+      var frames = this.ordered_frames();
+      for(i = 0; i < frames.length; i ++) {
+        var colors = frames[i].colors;
+        for(j = 0; j < colors.length; j ++) {
+          formatted_colors[j] = format_color(colors[j]);
+        } 
+        formatted_frames[i] = "[ " + formatted_colors.join(",") + " ]";
       } 
-      formatted_frames[i] = "[ " + formatted_colors.join(",") + " ]";
-    } 
-    var output = "[ " + formatted_frames.join(" ,") + " ]";
-    return output;
-  };
+      var output = "[ " + formatted_frames.join(" ,") + " ]";
+      return output;
+    };
 
-} // end diagram
+  } // end diagram
 
-function format_color(color) 
-{
-  var c = hexToRgb(color);
-  $foo = color;
-  return "[" + c.r + ", " + c.g + ", " + c.b + "]";
-}
+  function format_color(color) 
+  {
+    var c = hexToRgb(color);
+    $foo = color;
+    return "[" + c.r + ", " + c.g + ", " + c.b + "]";
+  }
 
-//------------------------------
-// Frame
-// belongs_to diagram
-// has_many colors
+  //------------------------------
+  // Frame
+  // belongs_to diagram
+  // has_many colors
 
   function frame(diagram, target, id, append_after)
   {
