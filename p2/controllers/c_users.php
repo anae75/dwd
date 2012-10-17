5863079078bf1c64c8c9435d5d1f0732f6a288dd
ana <ana.devel@gmail.com>
2012-10-16 22:26:07 -0400

    example user signup added

--- p2/controllers/c_users.php
+++ p2/controllers/c_users.php
@@ -1,35 +1,61 @@
 <?php
 class users_controller extends base_controller {
 
-        public function __construct() {
-                parent::__construct();
-                echo "users_controller construct called<br><br>";
-        } 
-        
-        public function index() {
-                echo "Welcome to the users's department";
-        }
-        
-        public function signup() {
-                echo "This is the signup page";
-        }
-        
-        public function login() {
-                echo "This is the login page";
-        }
-        
-        public function logout() {
-                echo "This is the logout page";
-        }
-        
-        public function profile($user_name = NULL) {
-                
-                if($user_name == NULL) {
-                        echo "No user specified";
-                }
-                else {
-                        echo "This is the profile for ".$user_name;
-                }
-        }
-                
+  public function __construct() 
+  {
+    parent::__construct();
+    echo "users_controller construct called<br><br>";
+  } 
+
+  public function index() {
+    echo "Welcome to the users's department";
+  }
+
+  public function signup() 
+  {
+    # Setup view
+    $this->template->content = View::instance('v_users_signup');
+    $this->template->title   = "Signup";
+    # Render template
+    echo $this->template;
+  }
+
+  public function p_signup() {
+
+    # Encrypt the password  
+    $_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+
+    # More data we want stored with the user        
+    $_POST['created']  = Time::now();
+    $_POST['modified'] = Time::now();
+    $_POST['token']    = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
+
+    # Dump out the results of POST to see what the form submitted
+    print_r($_POST);
+
+    # Insert this user into the database
+    $user_id = DB::instance(DB_NAME)->insert("users", $_POST);
+
+    # For now, just confirm they've signed up - we can make this fancier later
+    echo "You're signed up";
+  }        
+
+  public function login() {
+          echo "This is the login page";
+  }
+
+  public function logout() {
+          echo "This is the logout page";
+  }
+
+  public function profile($user_name = NULL) {
+          
+          if($user_name == NULL) {
+                  echo "No user specified";
+          }
+          else {
+                  echo "This is the profile for ".$user_name;
+          }
+  }
+
 } # end of the class
