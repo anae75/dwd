6a6c14ebbe0d5a7918db47b594eef958a934fd92
ana <ana.devel@gmail.com>
2012-10-17 19:02:33 -0400

    overriding default salt

--- p2/config/config.php
+++ p2/config/config.php
@@ -42,5 +42,5 @@ A email designated to receive messages from the server. Examples:
 	//define('SMTP_PASSWORD', '');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	define('PASSWORD_SALT', '3457DFc43#$!$#%(8fhgljgd');
+	define('TOKEN_SALT', '#%&^)JOIL;HFPIEFD8F!$#NP8FXVG');
