2d926a6eaf034c40fe7fd00eabfa9c3bf5b1cda5
ana <ana.devel@gmail.com>
2012-10-25 19:07:59 -0400

    fixing some markup issues

--- p2/views/v_index_index.php
+++ p2/views/v_index_index.php
@@ -1,37 +1,37 @@
-
 <div class=centering>
 
-
-<div id=welcome title="Welcome to MMMMicroblogger">
-  <div class=logo>
-  Welcome to <br><span class=name>MMMMicroblogger</span>
+  <div id=welcome title="Welcome to MMMMicroblogger">
+    <div class=logo>
+    Welcome to <br><span class=name>MMMMicroblogger</span>
+    </div>
+    Get started!
+    <a href="/users/login">Log In</a> or <a href="/users/signup">Sign Up</a>
   </div>
-  Get started!
-  <a href="/users/login">Log In</a> or <a href="/users/signup">Sign Up</a> </li>
-</div>
 
-<div id=recent_posts class=stream>
-<h2>Here's what people are saying:</h2>
-<dl>
-<? foreach($posts as $p) { ?>
-  <dt> 
-    <span class=user><?= htmlspecialchars($p->first_name) ?></span>
-    at <?= date('D M d, Y, h:ia', $p->created) ?>
-  </dt>
-  <dd>
-    <?= htmlspecialchars($p->text) ?>
-  </dd>
-<? } ?>
-</dl>
-</div>
-
-<div id=context_help class=help>
-  <span class="title">Welcome to MMMMicroblogger!</span>
+  <div id=recent_posts class=stream>
+  <h2>Here's what people are saying:</h2>
   <dl>
-  <dt>Why?</dt>
-  <dd>This is where you can keep up with all the latest news from friends and interesting people around the internet.</dd>
-  <dt>Get Started</dt>
-  <dd>If you have an account, click "Log In" to go to your home page.  Or "Sign Up" to create an account now.</dd>
+  <? foreach($posts as $p) { ?>
+    <dt> 
+      <span class=user><?= htmlspecialchars($p->first_name) ?></span>
+      at <?= date('D M d, Y, h:ia', $p->created) ?>
+    </dt>
+    <dd>
+      <?= htmlspecialchars($p->text) ?>
+    </dd>
+  <? } ?>
   </dl>
-  Remember, you can always click "Need Help?" to get help on whatever page you're on.
+  </div>
+
+  <div id=context_help class=help>
+    <span class="title">Welcome to MMMMicroblogger!</span>
+    <dl>
+    <dt>Why?</dt>
+    <dd>This is where you can keep up with all the latest news from friends and interesting people around the internet.</dd>
+    <dt>Get Started</dt>
+    <dd>If you have an account, click "Log In" to go to your home page.  Or "Sign Up" to create an account now.</dd>
+    </dl>
+    Remember, you can always click "Need Help?" to get help on whatever page you're on.
+  </div>
+
 </div>
