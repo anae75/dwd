b673e43f11301e703d08abfa13579eba7b725e4e
ana <ana.devel@gmail.com>
2012-10-24 21:51:00 -0400

    adding validation and formatting for posts_create and streams_create

--- p2/views/v_posts_create.php
+++ p2/views/v_posts_create.php
@@ -1,10 +1,23 @@
 <div class=centering>
-  <div class=form_content style="width: 300px">
+  <div class=form_content style="width: 425px">
     <h2>Post Something!</h2>
     <form id=create_post_form method='POST' action='/posts/p_create'>
-            <textarea form=create_post_form rows="4" cols="50" name="text" maxlength=200 > </textarea>
+            <textarea form=create_post_form rows="4" cols="50" name="text" maxlength=160 > </textarea>
             <br><br>
             <button type='submit'>Submit</button>
     </form>
   </div>
 </div>
+
+<script>
+  $(document).ready(function() {
+    $("#create_post_form").validate({
+      rules: {
+        text: {
+          required: true,
+          maxlength: 150
+        }
+      }
+    });
+  });
+</script>
--- p2/views/v_streams_create.php
+++ p2/views/v_streams_create.php
@@ -1,12 +1,30 @@
-<form id=create_stream_form method='POST' action='/streams/p_create'>
+<div class=centering>
+  <div class=form_content style="width: 425px">
+    <h2>Create a New Stream</h2>
+    <form id=create_stream_form method='POST' action='/streams/p_create'>
+      <label>Name:</label>
+      <input type"text" name="name" cols=50>
+      <br><br>
+      <label>description:</label>
+      <textarea form=create_stream_form rows="4" cols="50" name="description" maxlength=210 > </textarea>
+      <br><br>
+      <button type='submit'>Submit</button>
+    </form>
+  </div>
+</div>
 
-        <label>Name:</label>
-        <input type"text" name="name">
-        <br><br>
-        <label>description:</label>
-        <textarea form=create_stream_form rows="4" cols="50" name="description" maxlength=255 > </textarea>
-        <br><br>
-        <input type='submit' value="Submit">
-
-</form>
-
+<script>
+  $(document).ready(function() {
+    $("#create_stream_form").validate({
+      rules: {
+        name: {
+          required: true,
+          maxlength: 50
+        },
+        description: {
+          maxlength: 200
+        }
+      }
+    });
+  });
+</script>
