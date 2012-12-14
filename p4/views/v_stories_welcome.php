8f587b47d22d745107feac62e6b2121832f31bc6
ana <ana.devel@gmail.com>
2012-12-14 18:22:27 -0500

    forgot to check in the view

---
+++ p4/views/v_stories_welcome.php
@@ -0,0 +1,38 @@
+<div class=centering>
+
+  <div id=introduction>
+
+  <p> <h2>Welcome, commuter!</h2>
+  </p>
+
+  <p> "Lost on the MBTA" is a user-expandable online cartoon.  Your hero and his or her
+  brave companions will go on an adventure into depths of the T.   Along the way you 
+  may be asked to draw or add dialog to help the story along.  </p>
+
+  <p> But be careful --even underground there are hidden cameras recording everything you
+  do.  Even after you have completed your adventure, your character, drawings and dialog
+  will live on, accompanying other commuters on their trek through the tunnels.  The
+  companions you choose on your journey were once the heroes of someone else's story.
+  </p>
+
+  <p>
+  Let's get started!
+  <? if($story) { ?>
+  It looks like you have an unfinished adventure.
+  <a href="/stories/next_scene">Continue the Current Adventure</a> or
+  <? } ?>
+  <a href="/stories/new_story">Begin a New Adventure</a>
+  </p>
+
+  </div>
+
+  <div id=context_help class=help>
+    <span class="title">Welcome to "Lost... on the MBTA"</span>
+    <dl>
+    <dt>Why?</dt>
+    <dd>Because if you don't laugh on the T, you'll cry.</dd>
+    </dl>
+    Remember, you can always click "Need Help?" to get help on whatever page you're on.
+  </div>
+
+</div>
