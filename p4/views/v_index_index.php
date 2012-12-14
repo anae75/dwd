b659fde913ab81234b4168578b77ae1c4fbd3d79
ana <ana.devel@gmail.com>
2012-12-14 11:41:13 -0500

    added cover image

--- p4/views/v_index_index.php
+++ p4/views/v_index_index.php
@@ -2,10 +2,10 @@
 
   <div id=welcome >
     <div class=logo>
-      Image goes here.
+      <img src="/images/group_01.png">
     </div>
-    Get started!
-    <a href="/users/login">Log In</a> or <a href="/users/signup">Sign Up</a>
+    <h2> Are you strong enough to survive a day on the T?  </h2>
+    <h2>Find out!  <a href="/users/login">Log In</a> or <a href="/users/signup">Sign Up</a></h2>
   </div>
 
   <div id=context_help class=help>
